a1ce4e92c0b94fc451e7fa7843d66f39acaf6d04
kyy8-droid <kymoorzhusain@gmail.com>
2022-06-20 20:28:20 +0700

    commit pertama

---
+++ script.js
@@ -0,0 +1,32 @@
+function getResult() {
+    const namaPertama = document.getElementById("couple-nama-pertama").value;
+    const namaKedua = document.getElementById("couple-nama-kedua").value;
+    fetchData(namaPertama, namaKedua);
+}
+
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '5d9b0c9a47msh79b8c2d26e39314p19c3e2jsne094293021ef',
+		'X-RapidAPI-Host': 'love-calculator.p.rapidapi.com'
+	}
+};
+
+async function fetchData() {
+	let result = await fetch(`https://love-calculator.p.rapidapi.com/getPercentage?sname=Alice&fname=John`, options)
+		.then(response => response.json())
+		.then(result => {
+            console.log(result.fname)
+            // console.log(`nama pertama: ${namaPertama}`);
+            // console.log(`nama kedua: ${namaKedua}`);
+
+            const namaPertama = document.getElementById("couple-nama-pertama").value;
+            const namaKedua = document.getElementById("couple-nama-kedua").value;
+            console.log(namaPertama);
+            console.log(namaKedua);
+            
+        })
+		.catch(err => console.error(err))
+};
+
