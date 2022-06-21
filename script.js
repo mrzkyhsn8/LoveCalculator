91ee7e7f416da201fbc43bf114b842016f76b37f
kyy8-droid <kymoorzhusain@gmail.com>
2022-06-21 22:23:38 +0700

    first upload

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 const options = {
 	method: 'GET',
 	headers: {
@@ -9,15 +35,46 @@ const options = {
 async function fetchData() {
 	const namaPertama = document.getElementById("couple-nama-pertama").value;
     const namaKedua = document.getElementById("couple-nama-kedua").value;
-    const persentase = document.getElementById("result-persentasi");
+	const resultNama = document.getElementById("result-nama");
+    const persentasi = document.getElementById("result-persentasi");
     const message = document.getElementById("result-pesan");
 	let data = []
 	let result = await fetch(`https://love-calculator.p.rapidapi.com/getPercentage?sname=${namaPertama}&fname=${namaKedua}`, options)
 		.then(response => response.json())
 		.then(result => {
-				persentase.innerHTML = result.percentage;
-				message.innerHTML = result.result;
+
+			const persen = result.percentage;
+			const pesan = result.result;
+			
+			// tampil nama
+			resultNama.innerHTML = `<div>${namaPertama} & ${namaKedua}</div>`;
+			
+			// tampil hasil persen dan
+			let c=0;
+			function viewResult() {
+				setTimeout( () => {
+					persentasi.innerHTML = `
+						<div>${c} %</div>
+					`
+					c++;
+					if ( c <= persen ) {
+						viewResult();
+					} else {
+						message.innerHTML = `<div>${pesan}</div>`;
+					}
+				}, 100);
+				clearForm();
+			}
+
+			// kosongkan form input
+			function clearForm() {
+				document.getElementById("couple-nama-pertama").value = '';
+				document.getElementById("couple-nama-kedua").value = '';
+			}
+
+			viewResult();
         })
 		.catch(err => {return data = [...data, err]});
 	return result;
 };
+
